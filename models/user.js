ce30cafaa75eb8090cec13642a4f3af889c86dde
atomic-Samurai <akshatkumar6932@gmail.com>
2021-10-06 12:24:29 +0530

    db working

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 
-const userSchema=new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email:{
         type:String,
         required:true,
@@ -20,6 +20,6 @@ const userSchema=new mongoose.Schema({
 });
 
 
-const User =mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports =User;
+module.exports = User;
